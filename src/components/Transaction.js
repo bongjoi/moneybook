ab152c7f04634f77c1f3d54a963be88cbff03f36
bongjoi <bong5176@gmail.com>
2020-07-16 18:00:02 +0900

    오류 수정

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { currencyFormatter } from '../utils/currencyFormatter'
 
 export const Transaction = ({ text, amount }) => {
   const sign = amount < 0 ? '-' : '+'
+  const currency = currencyFormatter(Math.abs(amount))
 
   return (
     <li
       className={
-        'transaction__item ' + (amount < 0 ? 'transaction__item--plus' : 'transaction__item--minus')
+        'transaction__item ' + (amount < 0 ? 'transaction__item--minus' : 'transaction__item--plus')
       }>
       {text}
       <span>
         {sign}
-        {Math.abs(amount)} 원
+        {currency} 원
       </span>
       <button className="btn--delete">x</button>
     </li>
