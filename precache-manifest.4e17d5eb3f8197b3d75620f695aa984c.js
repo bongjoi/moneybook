c8932d35844bb3c0bd611a40025b89ba0f120817
bongjoi <bong5176@gmail.com>
2020-07-16 23:42:46 +0900

    Updates

---
+++ precache-manifest.4e17d5eb3f8197b3d75620f695aa984c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38eef0efa68dcab17b8bcecb11813a6",
+    "url": "/moneybook/index.html"
+  },
+  {
+    "revision": "345d949e635e1e82a275",
+    "url": "/moneybook/static/css/main.412ca259.chunk.css"
+  },
+  {
+    "revision": "5bb6c481153c723dd58e",
+    "url": "/moneybook/static/js/2.3559adb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moneybook/static/js/2.3559adb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345d949e635e1e82a275",
+    "url": "/moneybook/static/js/main.d2740fc5.chunk.js"
+  },
+  {
+    "revision": "70536518be4cb4f0309a",
+    "url": "/moneybook/static/js/runtime-main.f938a012.js"
+  }
+]);
